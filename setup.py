8681051874afb08e54229b3332420743bb53c4e9
Sanne Boels <77999025+Sanneeeee@users.noreply.github.com>
2025-05-09 14:24:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+efrom setuptools import setup, find_packages
 
 setup(
-    name='growatt-api',
+    name='growatt-api-devices',
     version='0.1.3',
     packages=find_packages(),
     install_requires=[
